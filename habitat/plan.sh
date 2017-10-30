752ccbdcd98a61a94aaad3fc26044c33b85278c9
Franklin Webber <franklin.webber@gmail.com>
2017-10-29 23:05:46 -0500

    Adds Habitat file to build package
    It does not run yet.
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

---
+++ habitat/plan.sh
@@ -0,0 +1,17 @@
+pkg_name=national-parks
+pkg_origin=franklinwebber
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('Apache-2.0')
+pkg_build_deps=( core/maven )
+
+do_build() {
+    cp -r src $HAB_CACHE_SRC_PATH/$pkg_dirname/src
+    cp pom.xml $HAB_CACHE_SRC_PATH/$pkg_dirname/pom.xml
+    cd $HAB_CACHE_SRC_PATH/$pkg_dirname
+    mvn package
+}
+
+do_install() {
+    cp $HAB_CACHE_SRC_PATH/$pkg_dirname/target/$pkg_name.war $pkg_prefix/
+}
